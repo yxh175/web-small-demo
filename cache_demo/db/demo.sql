5e0bb604a016b9d28d0977013b67fce139ab73ea
yxh175 <1759788845@qq.com>
2023-10-10 22:06:58 +0800

    add: sql语句

---
+++ cache_demo/db/demo.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA cache_demo;
+use cache_demo;
+DROP TABLE if exists products;
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    count INT NOT NULL
+);
